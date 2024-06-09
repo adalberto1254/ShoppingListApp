66160ffb44d385fa5dbcffee5dcf758d71ab2859
Adalberto <asouza@wayfair.com>
2024-06-08 21:58:30 -0400

    Shopping List App, but no storage yet

---
+++ app/src/main/java/com/example/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myshoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyShoppingListAppTheme {
+                ShoppingList()
+            }
+        }
+    }
+}
